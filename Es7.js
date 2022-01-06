1e9df367c2ebedef72bfd0c9a990461fdb14925c
Hayder7Aly <007harryjutt@gmail.com>
2022-01-06 11:57:24 +0500

    learn basics of js

---
+++ Es7.js
@@ -0,0 +1,35 @@
+// ES7 I N JS 
+
+
+// 1. Array.prototype.includes
+
+// it returns true if the element in array else return false 
+
+const colors = ['red','yellow','blue','white','pink']
+const ispresent = colors.includes('blue')
+console.log(ispresent)
+
+// 2. Exponentiation operator 
+
+
+const mynumber = 2**3
+console.log(mynumber)
+
+
+
+
+
+// let myObj = {
+//     firstName : 'Hayder',
+//     lastName : 'Aly',
+//     age : 16,
+//     education : '9th A',
+// }
+// let myTodeArray = [
+//     ['hayder' , 'aly'],
+//     ['programming' , 'javascript'],
+//     ['interface' , 'smoothly'],
+//     ['consult' , 'terminty']
+// ]
+// console.log(Object.entries(myObj))
+// console.log(Object.fromEntries(myTodeArray));
